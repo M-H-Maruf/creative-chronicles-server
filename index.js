c6a2dee9427932b442931c4fd82cc27acf1221df
M-H-Maruf <maruf.nightwalker@gmail.com>
2023-11-06 20:51:54 +0600

    get all blog data

--- index.js
+++ index.js
@@ -29,6 +29,16 @@ async function run() {
         // Connect the client to the server	(optional starting in v4.7)
         // await client.connect();
 
+        // creative chronicles database
+        const creativeChroniclesDatabase = client.db('creativeChronicles');
+
+        // blogs collection
+        const blogCollection = creativeChroniclesDatabase.collection('blogs')
+        app.get('/blogs', async (req, res) => {
+            const cursor = blogCollection.find();
+            const result = await cursor.toArray();
+            res.send(result);
+        })
 
 
         // Send a ping to confirm a successful connection
